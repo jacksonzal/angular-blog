1e658e89bacd4c8fb2d7daa1aaf7469d25637ceb
Jackson Zaloumis <j.g.zaloumis@gmail.com>
2019-04-29 15:57:55 +1000

    All Tests to pass

--- apps/blog/src/app/auth.guard.spec.ts
+++ apps/blog/src/app/auth.guard.spec.ts
@@ -1,15 +1,29 @@
 import { TestBed, async, inject } from '@angular/core/testing';
+import { BehaviorSubject } from 'rxjs';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { AuthService } from './auth.service';
 
 import { AuthGuard } from './auth.guard';
 
 describe('AuthGuard', () => {
+  let authServiceStub: Partial<AuthService>;
+
   beforeEach(() => {
+    authServiceStub = {
+      isAuthenticated: new BehaviorSubject(true)
+    };
+
     TestBed.configureTestingModule({
-      providers: [AuthGuard]
+      imports: [RouterTestingModule],
+      providers: [
+        AuthGuard,
+        { provide: AuthService, useValue: authServiceStub }
+      ]
     });
   });
 
-  it('should ...', inject([AuthGuard], (guard: AuthGuard) => {
+  it('should', inject([AuthGuard], (guard: AuthGuard) => {
     expect(guard).toBeTruthy();
   }));
 });
--- apps/blog/src/app/header/header.component.spec.ts
+++ apps/blog/src/app/header/header.component.spec.ts
@@ -1,16 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BehaviorSubject } from 'rxjs';
+
+import { AuthService } from '../auth.service';
 
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let authServiceStub: Partial<AuthService>;
 
   beforeEach(async(() => {
+    authServiceStub = {
+      isAuthenticated: new BehaviorSubject(true)
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule],
+      declarations: [HeaderComponent],
+      providers: [{ provide: AuthService, useValue: authServiceStub }]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
